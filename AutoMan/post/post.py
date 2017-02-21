3a36b0443d2e9391e4907c1ef9081abd8d0f3c76
System Administrator <root@macdeMacBook-Pro-54.local>
2017-02-21 15:52:30 +0800

    add pyc file

--- AutoMan/post/post.py
+++ AutoMan/post/post.py
@@ -22,14 +22,54 @@ def postJson(path,url):
 
 	req.add_header("Content-Type","application/json")
 	req.add_header("appType","true")
-	print "++++++++++++++++++++"
-	print values
-	print url
+#	print "++++++++++++++++++++"
+#	print values
+#	print url
 	response = urllib2.urlopen(req)
 
 	page = response.read()
 
-	print page
+#	print page
 
 	return page
 
+def postHeader(head,path,url):
+	if ""==path or ""==url:
+		return
+	
+	values = ""
+	jstr = ""
+	header = ""
+
+	with open(path,"r") as f:
+		for line in f.readlines():
+			jstr = jstr + line.strip()
+	
+	with open(head,"r") as f:
+		for line in f.readlines():
+			header = header + line.strip()
+
+	values = json.loads(jstr)#complie json body
+	jdata = json.dumps(values)
+
+	jhead = json.loads(header)#compile  request header
+	jkey = jhead.keys()
+	jvalue = jhead.values()
+
+	req = urllib2.Request(url,jdata)
+
+	jsize = len(jkey)
+	
+	for i in jsize:
+		req.add_header(jkey[i],jvalue[i])#build request header
+
+#	req.add_header("Content-Type","application/json")
+#	req.add_header("appType","true")
+#	print values
+#	print url
+	response = urllib2.urlopen(req)
+
+	page = response.read()
+
+#	print page
+	return page
--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 python manage.py runserver
 
-#python manage.py runserver 0.0.0.0:8000
